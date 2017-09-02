d6f9e326343f33e5a2c14c209a1230ec560a9c51
Apimook <apimook555plug@gmail.com>
2017-09-02 16:47:49 +0700

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end("<h2 style='color:red'>Hello World from apimook yoopoo </h2> <h2 style='color:red'>5711403452</h2>");
-}).listen(8080);
